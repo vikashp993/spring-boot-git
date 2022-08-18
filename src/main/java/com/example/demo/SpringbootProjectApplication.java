b3e367863249d43d51ba76d46c5513cf35cb076c
vikashp993 <vikashp993@gmail.com>
2022-08-19 01:15:27 +0530

    first commit

--- src/main/java/com/example/demo/SpringbootProjectApplication.java
+++ src/main/java/com/example/demo/SpringbootProjectApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootProjectApplication.class, args);
-	}
+	
+	SpringApplication.run(SpringbootProjectApplication.class, args);
+	
+    void myMethod(){
+}
+
+}
 
 }
